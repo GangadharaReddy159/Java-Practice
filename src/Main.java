24963b416ad70d445fac582bd47db5083ce05bd7
Venkat <venkat@git.com>
2026-03-10 16:57:11 +0530

    added Person class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        Person venkat = new Person("Venkat", 24, "Vijayawada");
+        System.out.println(venkat);
     }
 }
