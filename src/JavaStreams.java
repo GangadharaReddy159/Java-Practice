7463a654cbe2d1f03a10293788031761c44c0260
Venkat <venkat@git.com>
2026-04-02 09:09:20 +0530

    added some stream methods

--- src/JavaStreams.java
+++ src/JavaStreams.java
@@ -1,12 +1,13 @@
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static java.util.Arrays.stream;
 
 public class JavaStreams {
-    public static void main(String[] args) {
+    public static <Employee> void main(String[] args) {
         int[] nums = {1,2,3,4,5};
         String[] words = {"Hello", "World", "Java", "Streams"};
 
@@ -21,6 +22,29 @@ public class JavaStreams {
         //sort names in descending order of length
         List<String> DescendingNames = stream(names).sorted(Comparator.comparing(String::length).reversed()).collect(Collectors.toList());
         System.out.println(DescendingNames);
+
+        List<String> myNames = List.of("venkat", "Bablu", "Gangadhara", "Reddy");
+        List<String> sortedOnSecondChar = myNames.stream().sorted(Comparator.comparing(s->s.charAt(1))).collect(Collectors.toList());
+        System.out.println(sortedOnSecondChar);
+
+        //create a list of 6 employees with name, age, salary and city, then sort them by age and print the sorted list.
+        List<Person> employees = List.of(
+            new Person("Alice", 30, "New York"),
+            new Person("Bob", 25,  "Los Angeles"),
+            new Person("Charlie", 35, "Chicago"),
+            new Person("David", 28,  "Houston"),
+            new Person("Eve", 32,  "Phoenix"),
+            new Person("Frank", 27,  "Philadelphia")
+        );
+
+        double avgAge = employees.stream().mapToInt(Person::getAge).average().orElse(0);
+        System.out.println("Average Age: "+avgAge);
+
+        Map<String, Long> employeeByCity = employees.stream().collect(Collectors.groupingBy(Person::getCity, Collectors.counting()));
+        System.out.println("Employees by City: "+employeeByCity);
+
+        Map<String, List<Person>> employeesByCity = employees.stream().collect(Collectors.groupingBy(Person::getCity, Collectors.toList()));
+        System.out.println("Employees by City: "+employeesByCity);
 
     }
 }
