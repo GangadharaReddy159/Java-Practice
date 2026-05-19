72f1c8c90dab51e44702c3490bc262bb72c064e6
Venkat <venkat@git.com>
2026-05-19 22:46:35 +0530

    added some new methods

--- src/JavaStrings.java
+++ src/JavaStrings.java
@@ -1,3 +1,10 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static com.sun.org.apache.xalan.internal.lib.ExsltSets.distinct;
+import static java.util.Arrays.stream;
+
 public class JavaStrings {
 
     public static void main(String[] args) {
@@ -12,5 +19,60 @@ public class JavaStrings {
         String reversed2 = new StringBuilder(s2).reverse().toString();
         boolean isPalindrome = s2.equals(reversed2);
         System.out.println("Is palindrome----------: " + isPalindrome);
+
+        //finding the duplicate or repeated characters in a string
+        String s3 = "programming";
+        List<Character> characters = s3.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(f->f.getValue()>1)
+                .map(Map.Entry::getKey)
+                .toList();
+
+        System.out.println(characters);
+
+        Map<Character, Long> charCout = s3.chars().mapToObj(c->(char)c).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet()
+                .stream()
+                .filter(f->f.getValue()>1)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        System.out.println(charCout);
+
+        String greet = "Hello World";
+        String r = stream(greet.split(" ")).map(s->new StringBuilder(s).reverse().toString()).collect(Collectors.joining(" "));
+        System.out.println(r);
+        String r2 = new StringBuilder(greet).reverse().toString();
+        System.out.println(r2);
+
+        List<Person> people = List.of(
+            new Person("Alice", 30, "New York"),
+            new Person("Bob", 25,  "Chicago"),
+            new Person("Charlie", 35, "Chicago"),
+            new Person("David", 28,  "Houston"),
+            new Person("Eve", 32,  "Phoenix"),
+            new Person("Frank", 27,  "Philadelphia")
+        );
+
+        List<Person> people2 = List.of(
+                new Person("Venky", 25, "Vijayawada"),
+                new Person("Bablu", 24, "Vijayawada"),
+                new Person("Gangadhara", 24, "Vijayawada"),
+                new Person("Reddy", 24, "Vijayawada")
+        );
+
+
+        List<List<Person>> totalPeople = List.of(people, people2);
+
+        List<Person> top3People = totalPeople.stream().flatMap(List::stream).sorted((p1, p2) -> Integer.compare(p2.getAge(), p1.getAge())).limit(3).toList();
+        System.out.println(top3People);
+
+        String hello = "Welcome to Java Programming!";
+        Optional<String> reversed1 = stream(hello.split(" ")).reduce((a, b)->b+" "+a);
+        System.out.println(reversed1.orElse(""));
+
+        //find the second highest age of the people in the list
+        Optional<Integer> secondHighestAge = totalPeople.stream().flatMap(List::stream).map(Person::getAge).distinct().sorted(Comparator.reverseOrder()).skip(1).findFirst();
+        System.out.println(secondHighestAge.orElse(null));
     }
 }
